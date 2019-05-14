5669dba14c6527a5a97f8b4e2685795006b5b242
Hyeona <danghyeona0113@gmail.com>
2019-05-14 13:25:57 +0900

    soltion

---
+++ 02CHAPTER_Solution1/02CHAPTER_Solution1/question1.cpp
@@ -0,0 +1,16 @@
+//2017-1학기 C++ 20163131 당현아
+#include <iostream>
+using namespace std;
+
+void main()
+{
+	int i, n = 0, sum = 0;
+
+	cout << "끝 수를 입력하세요>> ";
+	cin >> n;
+
+	for (i = 1; i <= n; i++)
+		sum += i;
+
+	cout << "1에서 " << n << "까지의 합은 " << sum << "입니다." << endl;
+}
