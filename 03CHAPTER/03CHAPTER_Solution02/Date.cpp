68b9227faec855f0a5976b0bcf6eed56f1da0b7d
Hyeona <danghyeona0113@gmail.com>
2019-06-01 22:55:44 +0900

    Date_date

---
+++ 03CHAPTER/03CHAPTER_Solution02/Date.cpp
@@ -0,0 +1,43 @@
+//2017-1학기 C++ 20163131 당현아 
+#include <iostream>
+#include "Date.h"
+
+Date::Date() {
+	year = 1;
+	month = 1;
+	day = 1;
+}
+
+Date::Date(int a, int b, int c) {
+	year = a;
+	month = b;
+	day = c;
+}
+
+Date::Date(string s) {
+	int fri, sec, leght;
+
+	leght = s.length();
+
+	fri = s.find('/');
+	sec = s.find('/', fri + 1);
+
+	year = stoi(s.substr(0, fri));
+	month = stoi(s.substr(fri + 1, sec - 1));
+	day = stoi(s.substr(sec + 1, leght));
+}
+
+void Date::show() {
+	cout << year << "³â" << month << "¿ù" << day << "ÀÏ" << endl;
+}
+
+int Date::getYear() {
+	return year;
+}
+int Date::getMonth() {
+	return month;
+}
+
+int Date::getDay() {
+	return day;
+}
