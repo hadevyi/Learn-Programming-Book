540b02a4c5043c7f0091f27584932bda3d442a21
Hyeona <danghyeona0113@gmail.com>
2019-06-01 23:00:21 +0900

    newRamdom_header

---
+++ 03CHAPTER/03CHAPTER_Solution04/Ramdom.h
@@ -0,0 +1,12 @@
+#ifndef RAMDOM
+#define RAMDOM
+//2017-1학기 C++ 20163131 당현아
+class Random {
+	int start_num;
+	int end_num;
+public:
+	Random();
+	int next();
+	int EvenRondom(int n1, int n2);
+};
+#endif // !RAMDOM
