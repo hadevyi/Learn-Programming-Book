eeee526f22b18f0d780c78666649d794481bbb22
Hyeona <danghyeona0113@gmail.com>
2019-06-03 09:48:11 +0900

    Oval_main

---
+++ 03CHAPTER/03CHAPTER_Solution07/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "Oval.h"
+using namespace std;
+
+void main() {
+	Oval a, b(3, 4);
+	a.set(10, 20);
+	a.show();
+	cout << b.getWidth() << ", " << b.getHeight() << endl;
+}
