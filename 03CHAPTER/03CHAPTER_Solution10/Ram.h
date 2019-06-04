adb98545f15c1b8e46b68e28f07b0e3a83ce1dcb
Hyeona <danghyeona0113@gmail.com>
2019-06-04 10:21:32 +0900

    Ram_header

---
+++ 03CHAPTER/03CHAPTER_Solution10/Ram.h
@@ -0,0 +1,14 @@
+#pragma once
+#ifndef RAM
+#define RAM
+class Ram {
+	char mem[100 * 1024];
+	int size;
+public:
+	Ram();
+	~Ram();
+	char read(int address);
+	void write(int address, char value);
+};
+#endif // !RAM
+
