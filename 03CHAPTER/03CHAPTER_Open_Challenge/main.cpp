1a656ac46e2a7a6b826d62c14e93609e517a99d8
Hyeona <danghyeona0113@gmail.com>
2019-06-06 22:24:01 +0900

    Exp_main

---
+++ 03CHAPTER/03CHAPTER_Open_Challenge/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include "Exp.h"
+using namespace std;
+
+void main() {
+	Exp a(3, 2);
+	Exp b(9);
+	Exp c;
+
+	cout << a.getValue() << ' ' << b.getValue() << ' ' << c.getValue() << endl;
+	cout << "a의 베이스 " << a.getBase() << ", 지수 " << a.getExp() << endl;
+
+	if (a.equals(b))
+		cout << "same" << endl;
+	else
+		cout << "not same" << endl;
+}
