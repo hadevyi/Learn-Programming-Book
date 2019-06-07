47d5ffe8e6a83f8b01497550b8b15710ec69abbe
Hyeona <danghyeona0113@gmail.com>
2019-06-07 10:21:22 +0900

    WordFame_WordFame

---
+++ 04CHAPTER/04CHAPTER_Open_Challenge/WordFame.h
@@ -0,0 +1,15 @@
+#pragma once
+#ifndef WORDFAME
+#define WORDFAME
+#include <string>
+
+class Word {
+	int num;
+	string bese;
+	string now;
+public:
+	Word();
+	void game();
+};
+
+#endif // !WORDFAME_H
