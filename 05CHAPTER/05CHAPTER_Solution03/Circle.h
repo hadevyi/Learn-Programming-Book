3aeaba3b5c90e9a93d75f77ef21a1a7d23b948fc
Hyeona <danghyeona0113@gmail.com>
2019-06-17 17:15:18 +0900

    Circle_header

---
+++ 05CHAPTER/05CHAPTER_Solution03/Circle.h
@@ -0,0 +1,12 @@
+#pragma once
+#ifndef CIRCLE
+#define CIRCLE
+class Circle {
+	int radius;
+public:
+	Circle(int r);
+	int getRadius();
+	void setRadius(int r);
+	void show();
+};
+#endif // ! CIRCLE
