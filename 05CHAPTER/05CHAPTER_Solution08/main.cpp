0aca4a6b8b8f62fcef1c22186a4b97509e07cd46
Dang-Hyeonah <danghyeon0113@gmail.com>
2019-06-20 22:50:19 +0900

    Book_main

---
+++ 05CHAPTER/05CHAPTER_Solution08/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "Book.h"
+using namespace std;
+
+void main() {
+	Book cpp("C++", 10000);
+	Book java = cpp;
+	java.set("Java", 12000);
+	cpp.show();
+	java.show();
+}
