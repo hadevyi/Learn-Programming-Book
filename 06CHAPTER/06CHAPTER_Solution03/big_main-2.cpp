fbc98c12ff0cb632b44be9c32ff04335aa0b0563
Dang-Hyeonah <dang0113@naver.com>
2019-06-27 23:41:36 +0900

    big_main_ver2

---
+++ 06CHAPTER/06CHAPTER_Solution03/big_main-2.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int big(int num1, int num2, int num3 = 100);
+
+int big(int num1, int num2, int num3)
+{
+	int big;
+
+	big = num1 > num2 ? num1 : num2;
+
+	if (big < num3)
+		return big;
+	else
+		return num3;
+}
+
+void main()
+{
+	int x = big(3, 5);
+	int y = big(300, 60);
+	int z = big(30, 60, 50);
+	cout << x << ' ' << y << ' ' << z << endl;
+}
