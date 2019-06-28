537db08dc78efe85749c4ce636f19ae01263d9ed
Dang-Hyeonah <dang0113@naver.com>
2019-06-28 19:03:24 +0900

    ArrayUtility_header

---
+++ 06CHAPTER/06CHAPTER_Solution05/ArrayUtility.h
@@ -0,0 +1,9 @@
+#pragma once
+#ifndef ARRAYUTILLITY
+#define ARRAYUTILLITY
+class ArrayUtility {
+public:
+	static void intToDouble(int source[], double dest[], int size);
+	static void doubleToInt(double source[], int dest[], int size);
+};
+#endif // !ARRAYUTILLITY
