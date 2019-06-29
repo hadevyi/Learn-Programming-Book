cce22950f30e26ab6a239bf64ebb9d8e1c061a78
Dang-Hyeonah <dang0113@naver.com>
2019-06-29 19:07:33 +0900

    ArrayUtility2_header

---
+++ 06CHAPTER/06CHAPTER_Solution06/ArrayUtility2.h
@@ -0,0 +1,9 @@
+#pragma once
+#ifndef ARRAYUTILITY2
+#define ARRAYUTILITY2
+class ArrayUtility2 {
+public:
+	static int* concat(int s1[], int s2[], int size);
+	static int* remove(int s1[], int s2[], int size, int& retSize);
+};
+#endif // !ARRAYUTILITY2
