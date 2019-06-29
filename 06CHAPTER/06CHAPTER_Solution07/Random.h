2660482b12914c367259013b18729975d7fddc69
Dang-Hyeonah <dang0113@naver.com>
2019-06-29 19:08:32 +0900

    Random_header

---
+++ 06CHAPTER/06CHAPTER_Solution07/Random.h
@@ -0,0 +1,11 @@
+#pragma once
+#ifndef RANDOM
+#define RANDOM
+class Random {
+public:
+	static void seed() { srand((unsigned)time(0)); }
+	static int nextInt(int min = 0, int max = 32767);
+	static char nextAlphabet();
+	static double nextDouble();
+};
+#endif // !RANDOM
