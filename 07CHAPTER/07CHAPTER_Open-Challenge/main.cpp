76ed73cac0fc23bd3fba6e14460a8477df959c76
Dang-Hyeonah <dang0113@naver.com>
2019-07-01 20:18:44 +0900

    Histogram_main

---
+++ 07CHAPTER/07CHAPTER_Open-Challenge/main.cpp
@@ -0,0 +1,9 @@
+#include "Histogram.h"
+
+void main()
+{
+	Histogram song("Wise men say, \nonly fools rush in But I can't help, \n");
+	song << "falling" << " in love with you." << "- by ";
+	song << 'k' << 'i' << 't';
+	!song;
+}
