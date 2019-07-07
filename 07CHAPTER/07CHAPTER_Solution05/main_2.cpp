f381a4009b331e516bafaa6bf7bd3c8d57a19884
Dang-Hyeonah <dang0113@naver.com>
2019-07-07 15:53:24 +0900

    Matrix_main_ver2

---
+++ 07CHAPTER/07CHAPTER_Solution05/main_2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include "Matrix_2.h"
+using namespace std;
+
+void main()
+{
+	Matrix a(1, 2, 3, 4), b(2, 3, 4, 5), c;
+	c = a + b;
+	a += b;
+	a.show();
+	b.show();
+	c.show();
+
+	if (a == c)
+		cout << "a and c are the same" << endl;
+	else
+		cout << "WHAT" << endl;
+}
