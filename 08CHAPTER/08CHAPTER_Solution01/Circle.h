073ffc4f8c52f816500ca4269db02fc28dd32484
Dang-Hyeonah <danghyeona0113@gmail.com>
2019-07-13 13:30:50 +0900

    Circle_header_in_NamedCircle

---
+++ 08CHAPTER/08CHAPTER_Solution01/Circle.h
@@ -0,0 +1,12 @@
+#pragma once
+#ifndef CIRCLE
+#define CIRCLE
+class Circle {
+	int radius;
+public:
+	Circle(int radius = 0) { this->radius = radius; }
+	int getRadius() { return radius; }
+	void setRadius(int radius) { this->radius = radius; }
+	double getArea() { return 3.14 * radius * radius; }
+};
+#endif // ! CIRCLE
