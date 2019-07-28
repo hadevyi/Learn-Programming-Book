f442f2d1483db0f5361e2f087def74758ed81f86
Dang-Hyeonah <danghyeona0113@gmail.com>
2019-07-29 01:02:40 +0900

    LoopAdder

---
+++ 09CHAPTER/09CHAPTER_Solution03/LoopAdder.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include "LoopAdder.h"
+using namespace std;
+
+void LoopAdder::read() {	//Enter x, y
+	cout << name << ":" << endl;
+	cout << "Add from the first to the second. Please enter two numbers.>> ";
+	cin >> x >> y;
+}
+
+void LoopAdder::write() {	//Result sum output
+	cout << "The sum of " <<x << " to " << y << " is " << sum << "." << endl;
+}
+
+void LoopAdder::run() {
+	read();	//Read x, y.
+	sum = calculate();	//Calculate around the loop.
+	write();	//Outputs the result sum.
+}
