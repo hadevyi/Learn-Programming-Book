8e88c6b4e54df7aca88d45de3692c86a6d2ee37b
Dang-Hyeonah <danghyeona0113@gmail.com>
2019-08-12 20:16:22 +0900

    update_oval

---
+++ 09CHAPTER/09CHAPTER_Solution08/Oval.h
@@ -0,0 +1,12 @@
+#pragma once
+#ifndef OVAL
+#define OVAL
+#include <string.h>
+#include "Shape.h"
+class Oval :public Shape {
+public:
+	Oval(string n = "", int w = 0, int h = 0) :Shape(n, w, h) { ; }
+	double getArea() { return width * height * 3.14; }	//Short radius * Long radius * circumference
+	string getName() { return name; }
+};
+#endif // !OVAL
