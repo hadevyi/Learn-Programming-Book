9859851e959df6f3731c40cba208331d9bdaa622
Dang-Hyeonah <danghyeona0113@gmail.com>
2019-08-12 20:16:34 +0900

    update_rect

---
+++ 09CHAPTER/09CHAPTER_Solution08/Rect.h
@@ -0,0 +1,12 @@
+#pragma once
+#ifndef RECT
+#define RECT
+#include <string.h>
+#include "Shape.h"
+class Rect :public Shape {
+public:
+	Rect(string n = "", int w = 0, int h = 0) :Shape(n, w, h) { ; }
+	double getArea() { return width * height; }
+	string getName() { return name; }
+};
+#endif // !RECT
