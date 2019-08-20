66a860c9579d902beeb83d4efead83c7074e8df1
Hyeon-ah <45550607+Dang-Hyeonah@users.noreply.github.com>
2019-08-20 22:46:15 +0900

    slide_javascript

--- slide.js
+++ slide.js
@@ -33,6 +33,7 @@ setInterval(autoplay,3000);//반복
 function slide_stop(){
 		var fade_idx = $(this).parent().index(); 
 		current = $(this).parent().index();//클릭한 버튼의 Index 를 받아서 그 다음 이미지부터 슬라이드 재생.
+	
 		if($('.slide_ul > li:animated').length >= 1) return false; //버튼 반복 클릭시 딜레이 방지
 		$('.slide_ul > li').fadeOut(400);
 		$('.slide_ul > li').eq(fade_idx).fadeIn(400);
