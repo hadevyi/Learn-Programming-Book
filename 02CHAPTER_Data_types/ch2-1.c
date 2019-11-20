bd2d0c40f5e96cba6eaa5eeeb00e4634e6f3311b
Dang-Hyeonah <danghyeona0113@gmail.com>
2019-11-20 11:41:31 +0900

    ch2-1

---
+++ 02CHAPTER_Data_types/ch2-1.c
@@ -0,0 +1,26 @@
+/*
+// 인제대학교 컴퓨터공학부 16학번 당현아 작성 (Email : dang0113@naver.com)
+#include <stdio.h>	// 전처리기 지시 영역
+
+
+//C 프로그램의 기본 구조를 보여주는 프로그램
+//밑변이 4이고 높이가 5인 사각형의 면적 구하기  
+
+int main()
+{
+	// 변수 선언
+	int area, width, height;
+
+	//자료 입력 : 여기서는 생략
+
+	//자료 처리
+	width = 4;
+	height = 5;
+	area = width*height;
+
+	//결과 출력
+	printf("면적 = %d\n", area);
+
+	return 0;
+}
+*/
