f67c7b3e979cdc5b223d323476b785769ea205f7
Dang-Hyeonah <danghyeona0113@gmail.com>
2019-11-24 22:30:36 +0900

    radius area

---
+++ 02CHAPTER_Data_types/radius_area.c
@@ -0,0 +1,13 @@
+// 인제대학교 컴퓨터공학부 16학번 당현아 작성 (Email : dang0113@naver.com)
+#include <stdio.h>
+
+int main()
+{
+	float radius = 10;
+	float area;
+	area = 3.14f*radius*radius;
+
+	printf("반지름이 %f인 원의 넓이는 %f입니다.\n", radius, area);
+
+	return 0;
+}
