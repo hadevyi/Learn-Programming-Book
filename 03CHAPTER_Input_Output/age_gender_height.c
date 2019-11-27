7efe7764e33608981a0141276a32e2eb8087af49
Dang-Hyeonah <danghyeona0113@gmail.com>
2019-11-27 10:20:05 +0900

    age gender height

---
+++ 03CHAPTER_Input_Output/age_gender_height.c
@@ -0,0 +1,25 @@
+//인제대학교 컴퓨터공학부 16학번 당현아 작성 (Email : dang0113@naver.com)
+#include <stdio.h>
+
+int main()
+{
+	int age;
+	char gender;
+	double height;
+
+	// 자료 입력 받기
+	printf("성별은? (남자라면 M 여자라면 F) ");
+	scanf("%c", &gender);
+	printf("나이는? ");
+	scanf("%d", &age);
+	printf("키는? ");
+	scanf("%lf", &height);
+
+	// 결과 출력하기
+	printf("\n==================\n");
+	printf("성별 : %c\n", gender);
+	printf("나이 : %d세\n", age);
+	printf("키 : %.1lfmcm\n", height);
+
+	return 0;
+}
