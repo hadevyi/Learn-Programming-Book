21183aae7f25be949306d6ed125a7323f65ee996
Dang-Hyeonah <danghyeona0113@gmail.com>
2019-12-01 23:44:40 +0900

    operator2

---
+++ 04CHAPTER_Operator/operator2.c
@@ -0,0 +1,21 @@
+//인제대학교 컴퓨터공학부 16학번 당현아 작성(Email : dang0113@naver.com)
+#include <stdio.h>
+
+int main()
+{
+	int x, y;
+
+	printf("정수1 >> "); scanf("%d", &x);
+	printf("정수2 >> "); scanf("%d", &y);
+	printf("\n");
+
+	printf("%d + %d = %d\n", x, y, x + y);
+	printf("%d - %d = %d\n", x, y, x - y);
+	printf("%d * %d = %d\n", x, y, x*y);
+	printf("%d / %d = %d\n", x, y, x / y);		// 정수/정수 결과는 정수
+	printf("%d %% %d = %d\n\n", x, y, x%y);		// %%는 '%'를 출력
+
+	printf("%d / %.1lf = %.1lf\n", x, 3.0, x / 3.0);
+
+	return 0;
+}
