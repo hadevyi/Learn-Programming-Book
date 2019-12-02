ba95f636e3ef59a0c5240551832f998055fc1b41
Dang-Hyeonah <danghyeona0113@gmail.com>
2019-12-02 22:06:02 +0900

    operator4

---
+++ 04CHAPTER_Operator/operator4.c
@@ -0,0 +1,19 @@
+//인제대학교 컴퓨터공학부 16학번 당현아 작성(Email : dang0113@naver.com)
+#include <stdio.h>
+
+int main()
+{
+	int x, y;
+
+	printf("두 정수를 입력하세요. >> ");
+	scanf("%d %d", &x, &y);
+
+	printf("(%d > %d)의 결과는 %d입니다.\n", x, y, x > y);
+	printf("(%d < %d)의 결과는 %d입니다.\n", x, y, x < y);
+	printf("(%d >= %d)의 결과는 %d입니다.\n", x, y, x >= y);
+	printf("(%d <= %d)의 결과는 %d입니다.\n", x, y, x <= y);
+	printf("(%d == %d)의 결과는 %d입니다.\n", x, y, x == y);
+	printf("(%d != %d)의 결과는 %d입니다.\n", x, y, x != y);
+
+	return 0;
+}
