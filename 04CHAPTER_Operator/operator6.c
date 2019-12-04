9a484ba88a50974ced58e0fa6ff38e006a9c0d76
Dang-Hyeonah <danghyeona0113@gmail.com>
2019-12-04 19:42:11 +0900

    operator6

---
+++ 04CHAPTER_Operator/operator6.c
@@ -0,0 +1,14 @@
+//인제대학교 컴퓨터공학부 16학번 당현아 작성(Email : dang0113@naver.com)
+#include <stdio.h>
+
+int main()
+{
+	int x = 5, y = 10, z;
+
+	z = (x += 2) || (y += 1);
+
+	printf("x = %d, y = %d\n", x, y);
+	printf("z = %d\n", z);
+
+	return 0;
+}
