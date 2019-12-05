ae79903834be31b691153dc62722f7cff4222fb5
Dang-Hyeonah <danghyeona0113@gmail.com>
2019-12-05 09:44:53 +0900

    operator7

---
+++ 04CHAPTER_Operator/operator7.c
@@ -0,0 +1,14 @@
+//인제대학교 컴퓨터공학부 16학번 당현아 작성(Email : dang0113@naver.com)
+#include <stdio.h>
+
+int main()
+{
+	int x;
+
+	printf("정수 입력 >> ");
+	scanf("%d", &x);
+
+	(x % 2 == 0) ? printf("%d는 짝수입니다.\n", x) : printf("%d는 홀수입니다.\n", x);
+
+	return 0;
+}
