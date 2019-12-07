1c59e496915f5697f0d7c1f92221fee7f0bbc434
Dang-Hyeonah <danghyeona0113@gmail.com>
2019-12-07 17:44:32 +0900

    operator10

---
+++ 04CHAPTER_Operator/operator10.c
@@ -0,0 +1,16 @@
+/*
+//인제대학교 컴퓨터공학부 16학번 당현아 작성(Email : dang0113@naver.com)
+#include <stdio.h>
+
+int main()
+{
+	int x = 5, y = 12;
+
+	printf("%d & %d = %d\n", x, y, x&y);
+	printf("%d | %d = %d\n", x, y, x | y);
+	printf("%d ^ %d = %d\n", x, y, x ^ y);
+	printf("~%d = %d\n", x, ~x);
+
+	return 0;
+}
+*/
