0a88747a62d4fe935d14ef775e5ec88ed72a7dfe
Dang-Hyeonah <danghyeona0113@gmail.com>
2020-01-07 00:27:21 +0900

    circle area

---
+++ 03CHAPTER/03CHAPTER_Example/circle_area.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+class Circle {
+public:
+	int radius;
+	double getArea();
+};
+
+double Circle::getArea(){
+	return 3.14*radius*radius;
+}
+
+int main() {
+	Circle donut;
+	donut.radius = 1;
+	double area = donut.getArea();
+	cout << "dount 면적은 " << area << endl;
+
+	Circle pizza;
+	pizza.radius = 30;
+	area = pizza.getArea();
+	cout << "pizza 면적은 " << area << endl;
+}
