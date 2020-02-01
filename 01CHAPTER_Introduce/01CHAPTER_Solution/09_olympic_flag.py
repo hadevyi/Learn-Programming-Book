e18220c732d821376aaa22c1d8f08fd1ec9b66fc
Dang-Hyeonah <danghyeona0113@gmail.com>
2020-02-01 22:48:54 +0900

    drawing olympic flag

---
+++ 01CHAPTER_Introduce/01CHAPTER_Solution/09_olympic_flag.py
@@ -0,0 +1,26 @@
+import turtle
+
+t = turtle.Turtle()
+t.shape("turtle")
+
+t.circle(100)
+
+t.up()
+t.goto(180, 0)
+t.down()
+t.circle(100)
+
+t.up()
+t.goto(360, 0)
+t.down()
+t.circle(100)
+
+t.up()
+t.goto(100, -150)
+t.down()
+t.circle(100)
+
+t.up()
+t.goto(250, -150)
+t.down()
+t.circle(100)
