703a5897ae43d4542157956398a82ba2f2932bd6
Dang-Hyeonah <danghyeona0113@gmail.com>
2020-02-01 22:45:19 +0900

    turtle circle and polygon

---
+++ 01CHAPTER_Introduce/01CHAPTER_Labs/turtle_circle_polygon.py
@@ -0,0 +1,18 @@
+import turtle
+
+t = turtle.Turtle()
+t.shape("turtle")
+
+t.forward(100)
+t.left(60)
+t.forward(100)
+t.left(60)
+t.forward(100)
+t.left(60)
+t.forward(100)
+t.left(60)
+t.forward(100)
+t.left(60)
+t.forward(100)
+
+t.circle(100)
