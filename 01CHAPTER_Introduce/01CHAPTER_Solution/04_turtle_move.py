eeb07563590a27cdd3cd1bed2ecf0e754ae7aed3
Dang-Hyeonah <danghyeona0113@gmail.com>
2020-02-01 22:47:10 +0900

    turtle forward left right

---
+++ 01CHAPTER_Introduce/01CHAPTER_Solution/04_turtle_move.py
@@ -0,0 +1,14 @@
+import turtle
+
+t = turtle.Turtle()
+t.shape("turtle")
+
+t.forward(100)
+t.left(90)
+t.forward(100)
+t.right(90)
+t.forward(100)
+t.right(90)
+t.forward(100)
+t.left(90)
+t.forward(100)
