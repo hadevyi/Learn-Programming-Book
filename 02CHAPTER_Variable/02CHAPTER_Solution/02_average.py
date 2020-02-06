4112c6683e440de2525cc3072472cec147d2b07e
Dang-Hyeonah <danghyeona0113@gmail.com>
2020-02-06 20:02:24 +0900

    average of integer

---
+++ 02CHAPTER_Variable/02CHAPTER_Solution/02_average.py
@@ -0,0 +1,7 @@
+num1 = int(input("첫 번째 숫자를 입력하시오 : "))
+num2 = int(input("두 번째 숫자를 입력하시오 : "))
+num3 = int(input("세 번째 숫자를 입력하시오 : "))
+
+avg = (num1 + num2 + num3) / 3
+
+print(num1, num2, num3, "의 평균은 ", avg, "입니다")
