3b49275365120a8ebf83a120c298d850ed8a1934
Dang-Hyeonah <danghyeona0113@gmail.com>
2020-02-13 15:39:52 +0900

    distance between tow dots

---
+++ 03CHAPTER_Calculrate/03CHATPER_Solution/04_distance_btw_two_dots.py
@@ -0,0 +1,8 @@
+x1 = int(input("x1 : "))
+y1 = int(input("y1 : "))
+x2 = int(input("x2 : "))
+y2 = int(input("y2 : "))
+
+distance = ((x1 - x2) ** 2 + (y1 - y2) ** 2) ** 0.5
+
+print("두점 사이의 거리 = ", distance)
