e34f151452cc39f9bad41f01235b67511dd54f4e
Dang-Hyeonah <danghyeona0113@gmail.com>
2020-02-21 19:36:37 +0900

    what is the list?

---
+++ 04CHAPTER_Data_type/04CHAPTER_Example/list.py
@@ -0,0 +1,12 @@
+slist = ['영어', '수학', '사회', '과학']
+
+list1 = [1, 2, 3, 4, 5]
+list2 = ["a", "b", "c", "d"]
+
+list_ = []
+list_.append(1)
+list_.append(2)
+list_.append(6)
+list_.append(3)
+list_.append(5)
+print(list_)
