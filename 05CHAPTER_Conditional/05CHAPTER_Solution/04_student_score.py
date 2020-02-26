7dda8658bf672981dcc612954f4d14f36a0275b0
Dang-Hyeonah <danghyeona0113@gmail.com>
2020-02-26 15:47:16 +0900

    student score

---
+++ 05CHAPTER_Conditional/05CHAPTER_Solution/04_student_score.py
@@ -0,0 +1,12 @@
+score = int(input("성적을 입력하시오 : "))
+
+if score >= 90 :
+    print("A학점입니다.")
+elif score >= 80 :
+    print("B학점입니다.")
+elif score >= 70 :
+    print("C학점입니다.")
+elif score >= 60 :
+    print("D학점입니다.")
+else :
+    print("F학점입니다.")
