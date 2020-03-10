965c0ec7d5dad6b1b550047bf2cc4ee7d32ffa22
Hyeona <45550607+Dang-Hyeona@users.noreply.github.com>
2020-03-10 22:17:55 +0900

    update_main

--- 02CHAPTER/02CHAPTER_Solution08/main.cpp
+++ 02CHAPTER/02CHAPTER_Solution08/main.cpp
@@ -1,30 +1,22 @@
-//2017-1학기 C++ 20163131 당현아
 #include <iostream>
+#include <string>
 using namespace std;
 
-int main()
+void main()
 {
-	char name_line[1000];
-	int i, j, leght, count = 0, cnt = 0;
+	char word[100] = {};
+	char max[100] = {};
 
-	cout << "Enter 5 names separated by ';'\n>> ";
-	cin.getline(name_line, 1000, '\n');
+	cout << "5명의 이름을 'j'으로 구분하여 입력하세요.\n>> ";
 
-	leght = strlen(name_line);
-
-	for (i = 0; i < 5; i++)	{
-		cout << i << " : ";
-
-		for (j = count; j < leght - 1; j++) {
-			if (name_line[j] == ';') {
-				cnt++;
-				break;
-			}
-
-			cout << name_line[j];
-			cnt++;
+	for (int i = 0; i < 5; i++) {
+		cin.getline(word, 100, ';');
+		cout << i << " : " << word << endl;
+		if (strlen(max) < strlen(word)) {
+			strcpy(max, word);
 		}
-		count = cnt;
-		cout << endl;
 	}
+
+	cout << "가장 긴 이름은 " << max << endl;
 }
+//Mozart;Elvis Presley;Jim Carry;Schubert;Dominggo;
