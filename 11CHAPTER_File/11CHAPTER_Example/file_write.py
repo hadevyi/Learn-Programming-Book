804ffe47260efcdd26509205eaf185177ddd1a09
Dang-Hyeonah <danghyeona0113@gmail.com>
2020-03-14 10:53:27 +0900

    file write

---
+++ 11CHAPTER_File/11CHAPTER_Example/file_write.py
@@ -0,0 +1,7 @@
+outfile = open("phones1.txt", "w", encoding="UTF8")
+
+outfile.write("홍길동 010-1234-5678\n")
+outfile.write("이철수 010-1234-5679\n")
+outfile.write("김영희 010-1234-5680\n")
+
+outfile.close()
