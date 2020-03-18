9aa9dcc474d5473fb05d59f1e9fd560b041b989f
Dang-Hyeonah <danghyeona0113@gmail.com>
2020-03-18 14:51:02 +0900

    calculation

---
+++ 02CHAPTER_Variable/02CHAPTER_Challenge/62_calculation.py
@@ -0,0 +1,7 @@
+num1 = int(input("첫 번째 정수를 입력하시오 : "))
+num2 = int(input("두 번째 정수를 입력하시오 : "))
+
+print("덧셈 : ", num1, "+" , num2, "=", num1 + num2)
+print("뺄셈 : ", num1, "-" , num2, "=", num1 - num2)
+print("곱셈 : ", num1, "*" , num2, "=", num1 * num2)
+print("나눗셈 : ", num1, "/" , num2, "=", num1 / num2)
