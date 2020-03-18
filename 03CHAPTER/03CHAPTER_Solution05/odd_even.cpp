1f581e0b86bff2264fad3b3c584c773a10e98ff9
Hyeona <45550607+Dang-Hyeona@users.noreply.github.com>
2020-03-18 14:49:25 +0900

    Update odd_even.cpp

--- 03CHAPTER/03CHAPTER_Solution05/odd_even.cpp
+++ 03CHAPTER/03CHAPTER_Solution05/odd_even.cpp
@@ -1,4 +1,3 @@
-//2017-1학기 C++ 20163131 당현아
 #include <cstdlib>
 #include <ctime>
 #include "odd_even.h"
@@ -7,23 +6,33 @@ Random::Random() {
 	srand((unsigned)time(0));
 }
 
+Random::Random(int n1, int n2) {
+	srand((unsigned)time(0));
+
+	start_num = n1;
+	end_num = n2;
+}
+
 int Random::EvenRandom() {
 	int n = rand();
 
+	while (true) {
+		int n = rand();
+
+		if (n % 2 == 0)
+			return n;
+	}
+
 	if (n % 2 == 0)
 		return n;
-	else
-		return n - 1;
 }
 
-int Random::OddRandom(int n1, int n2) {
-	start_num = n1;
-	end_num = n2;
+int Random::OddRandom() {
 
-	int n = n1 + (rand() % (n2 - n1 + 1));
+	while (true) {
+		int n = rand() % (end_num - start_num + 1) + start_num;
 
-	if (n % 2 == 1)
-		return n;
-	else
-		return n - 1;
+		if (n % 2 == 1)
+			return n;
+	}
 }
