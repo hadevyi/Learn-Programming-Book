29c756f85869b2eff3322747d47ba1780de40fa4
Dang-Hyeonah <danghyeona0113@gmail.com>
2020-03-21 20:09:59 +0900

    login password

---
+++ 05CHAPTER_Conditional/05CHAPTER_Challenge/148_login_password.py
@@ -0,0 +1,10 @@
+id = "ilovepython"
+pw = "123456"
+
+s = input("아이디를 입력하시오 : ")
+p = input("패스워드를 입력하시오 : ")
+
+if s == id and p == pw:
+    print("환영합니다.")
+else :
+    print("아이디/비밀번호를 찾을 수 없습니다.")
