1fb4994555708f02b857a976b9c411f4fc9a95e4
Dang-Hyeonah <danghyeona0113@gmail.com>
2020-04-22 22:21:25 +0900

    change name

--- 03CHAPTER_Input_Output/Example/3-03_age_gender_height.c
+++ 03CHAPTER_Input_Output/Example/3-03_age_gender_height.c
@@ -1,4 +1,3 @@
-//인제대학교 컴퓨터공학부 16학번 당현아 작성 (Email : dang0113@naver.com)
 #include <stdio.h>
 
 int main()
@@ -8,18 +7,18 @@ int main()
 	double height;
 
 	// 자료 입력 받기
+	printf("성별은? (남자라면 M 여자라면 F) ");
+	scanf("%c", &gender);
 	printf("나이는? ");
 	scanf("%d", &age);
 	printf("키는? ");
 	scanf("%lf", &height);
-	printf("성별은? (남자라면 M 여자라면 F) ");
-	gender = getchar();	// scanf("%c", &gender);
 
 	// 결과 출력하기
-	printf("\n====================\n");
+	printf("\n==================\n");
 	printf("성별 : %c\n", gender);
 	printf("나이 : %d세\n", age);
-	printf("키 : %.1lfcm\n", height);
+	printf("키 : %.1lfmcm\n", height);
 
 	return 0;
 }
