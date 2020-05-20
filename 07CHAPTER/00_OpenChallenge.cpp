e49e9f39f3509815ed06cb22c7bb10d8b9dfa1a9
Dang-Hyeonah <danghyeona0113@gmail.com>
2020-05-20 23:21:41 +0900

    openchallenge

--- 07CHAPTER/00_OpenChallenge.cpp
+++ 07CHAPTER/00_OpenChallenge.cpp
@@ -1,7 +1,15 @@
 #include <iostream>
 #include <string>
-#include "Histogram.h"
 using namespace std;
+
+class Histogram {
+	string word;
+public:
+	Histogram(string word);
+	Histogram& operator<<(string word);
+	Histogram& operator<<(char ch);
+	void operator!();
+};
 
 Histogram::Histogram(string word)
 {
@@ -53,4 +61,12 @@ void Histogram::operator!()
 
 		cout << endl;
 	}
+}
+
+void main()
+{
+	Histogram song("Wise men say, \nonly fools rush in But I can't help, \n");
+	song << "falling" << " in love with you." << "- by ";
+	song << 'k' << 'i' << 't';
+	!song;
 }
--- 07CHAPTER/07CHAPTER_Open-Challenge/main.cpp
+++
@@ -1,9 +0,0 @@
-#include "Histogram.h"
-
-void main()
-{
-	Histogram song("Wise men say, \nonly fools rush in But I can't help, \n");
-	song << "falling" << " in love with you." << "- by ";
-	song << 'k' << 'i' << 't';
-	!song;
-}
