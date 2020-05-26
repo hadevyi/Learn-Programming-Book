15f3c22764c99634a8a94e14f7e68ea563c3958e
Dang-Hyeona <eona1301@gmail.com>
2020-05-27 01:15:45 +0900

    widget example

---
+++ 07_Widget/01_radio_comboBox.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ComboBoxBrith
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            comboBox1.SelectedIndex = 7;
+            comboBox2.SelectedIndex = 2;
+            comboBox3.SelectedIndex = 0;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label5.Text = "";
+            String sData01 = textBox1.Text;
+
+            if (textBox1.Text == "")
+            {
+                label5.Text = "성명을 입력하세요!";
+            }
+            else if (radioButton1.Checked == false && radioButton2.Checked == false)
+            {
+                label5.Text = "성별을 선택하여주세요!";
+            }
+            else
+            {
+                if (radioButton1.Checked == true)
+                {
+                    label5.Text = sData01 + "님의 정보\n성별 : 남\n생년월일 " + comboBox1.SelectedItem + " 년 " + comboBox2.SelectedItem + " 월 " + comboBox3.SelectedItem + " 일";
+                }
+                else if (radioButton2.Checked == true)
+                {
+                    label5.Text = sData01 + "님의 정보\n성별 : 여\n생년월일 " + comboBox1.SelectedItem + " 년 " + comboBox2.SelectedItem + " 월 " + comboBox3.SelectedItem + " 일";
+                }
+
+            }
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int iData01 = comboBox3.SelectedIndex;
+            comboBox3.Items.Clear();
+
+            if (comboBox2.SelectedIndex == 1)
+            {
+                for (int i = 1; i < 29; i++)
+                {
+                    comboBox3.Items.Add(i.ToString());
+                }
+            } // 28일까지 나오는 2월
+            else if (comboBox2.SelectedIndex == 3 || comboBox2.SelectedIndex == 5 || comboBox2.SelectedIndex == 8 || comboBox2.SelectedIndex == 10)
+            {
+                for (int i = 1; i < 31; i++)
+                {
+                    comboBox3.Items.Add(i.ToString());
+                }
+            } // 30일까지 나오는 월
+            else
+            {
+                for (int i = 1; i < 32; i++)
+                {
+                    comboBox3.Items.Add(i.ToString());
+                }
+            } // 31일까지 나오는 월
+
+            comboBox3.SelectedIndex = iData01;
+        }
+    }
+}
---
+++ 07_Widget/04_listBox.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CarOptionSelecte
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            comboBox1.Items.Add("일시불");
+            comboBox1.Items.Add("무통장입금");
+            comboBox1.Items.Add("신용카드");
+            comboBox1.SelectedIndex = 0;
+            listBox1.Items.Add(" ");
+
+            label3.Image = Properties.Resources.car_0;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int iData01 = 0;
+            listBox1.Items.Clear();
+            listBox1.Items.Add("선택차종:");
+
+            if (radioButton1.Checked == true)
+            {
+                listBox1.Items.Add("      승용차");
+                label3.Image = Properties.Resources.car_1;
+
+                iData01 = iData01 + 100;
+            }
+            else if (radioButton2.Checked == true)
+            {
+                listBox1.Items.Add("      트럭");
+                label3.Image = Properties.Resources.car_2;
+
+                iData01 = iData01 + 80;
+            }
+            else if (radioButton3.Checked == true)
+            {
+                listBox1.Items.Add("      봉고차");
+                label3.Image = Properties.Resources.car_3;
+
+                iData01 = iData01 + 50;
+            }
+
+            listBox1.Items.Add("-----------");
+            listBox1.Items.Add("선택옵션:");
+
+            if (checkBox1.Checked == true)
+            {
+                listBox1.Items.Add("      썬루프");
+
+                iData01 = iData01 + 1;
+            }
+            if (checkBox2.Checked == true)
+            {
+                listBox1.Items.Add("      자동온도조절장치");
+
+                iData01 = iData01 + 2;
+            }
+            if (checkBox3.Checked == true)
+            {
+                listBox1.Items.Add("      GPS");
+
+                iData01 = iData01 + 3;
+            }
+            if (checkBox4.Checked == true)
+            {
+                listBox1.Items.Add("      도난방지시스템");
+
+                iData01 = iData01 + 2;
+            }
+            if (checkBox5.Checked == true)
+            {
+                listBox1.Items.Add("      에어백");
+
+                iData01 = iData01 + 10;
+            }
+
+            listBox1.Items.Add("-----------");
+            listBox1.Items.Add("결제방법:");
+
+            if (comboBox1.SelectedIndex == 0)
+            {
+                listBox1.Items.Add("      일시불");
+            }
+            else if (comboBox1.SelectedIndex == 1)
+            {
+                listBox1.Items.Add("      무통장입금");
+            }
+            else if (comboBox1.SelectedIndex == 2)
+            {
+                listBox1.Items.Add("      신용카드");
+            }
+
+            listBox1.Items.Add("-----------");
+            listBox1.Items.Add("견적금액:");
+            listBox1.Items.Add("      " + iData01 + "만원");
+            label6.Text = "견적가 : " + iData01 + "만원";
+        }
+    }
+}
