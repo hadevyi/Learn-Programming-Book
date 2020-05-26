1bfc3d5d083a84fdd7e73b98d968c610426452d0
Dang-Hyeona <eona1301@gmail.com>
2020-05-27 01:13:20 +0900

    string example

---
+++ 06_String/02_string_split.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StringSplit
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string sData01 = "홍길동,이순신;임꺽정 세종대왕";
+            string sData02 = ";, ";
+
+            String[] sData;
+            String[] sData0;
+
+            sData = sData01.Split(sData02.ToCharArray());
+            sData0 = sData01.Split(sData02.ToCharArray(), 3);
+
+            label4.Text = sData[0];
+            label5.Text = sData[1];
+            label6.Text = sData[2];
+            label7.Text = sData[3];
+
+            label11.Text = sData0[0];
+            label8.Text = sData0[1];
+            label9.Text = sData0[2];
+        }
+    }
+}
---
+++ 06_String/04_resident_registration_number.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PersonNumber
+{
+    public partial class Form1 : Form
+    {
+        string[] sData;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            sData = new string[13];
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string sData01 = textBox1.Text;
+                int sum = 0;
+                if (sData01.IndexOf("-") >= 0)
+                {
+                    label2.Text = "-빼고 입력해주세요.";
+                }
+                else if (sData01.Length != 13)
+                {
+                    label3.Text = "정확히 입력하세요.";
+                }
+                else
+                {
+                    for (int i = 0; i < 12; i++)
+                    {
+                        sum += int.Parse(sData01.Substring(i, 1)) * ((i % 8) + 2);
+                    }
+                    if (((11 - (sum % 11)) % 10) == int.Parse(sData01.Substring(12, 1)))
+                    {
+                        label3.Text = "올바른 주민번호 입니다.";
+                    }
+                    else
+                    {
+                        label3.Text = "정확히 입력하세요.";
+                    }
+                }
+            }
+            catch
+            {
+                label3.Text = "정확히 입력하세요.";
+            }
+        }
+    }
+}
