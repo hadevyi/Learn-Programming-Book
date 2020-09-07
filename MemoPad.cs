8910f8bd5ddd891e89712f7eae1490d09541a473
Hyeona <eona1301@gmail.com>
2020-09-07 20:08:13 +0900

    Update MemoPad.cs

--- MemoPad.cs
+++ MemoPad.cs
@@ -12,174 +12,61 @@ namespace MemoPadWithEdit
 {
     public partial class Form1 : Form
     {
-        bool tbcp = false;  // text box change poing 
-        bool fse = false;   // file save experience
-        string fsN = "";    // file save Name
+        bool isModified = false;
+        bool hasFileName = false;
+        string sFileName = "";
 
         public Form1()
         {
             InitializeComponent();
         }
 
-        private void 새로만들기NToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            if (tbcp == true)
-            {
-                DialogResult answer = MessageBox.Show("변경사항을 저장하시겠습니까", "저장", MessageBoxButtons.YesNo);
-
-                if (answer == DialogResult.Yes)
-                {
-                    if (saveFileDialog1.ShowDialog() == DialogResult.OK)
-                    {
-                        fsN = saveFileDialog1.FileName;
-                        System.IO.StreamWriter fs = new System.IO.StreamWriter(fsN, false, System.Text.Encoding.Default);
-                        fs.WriteLine(textBox1.Text);
-                        fs.Close();
-
-                        fse = true;
-                        tbcp = false;
-                    }
-
-                    textBox1.Text = "";
-                    fsN = "";
-                    fse = false;
-                    tbcp = false;
-                }
-                else
-                {
-                    textBox1.Text = "";
-                    fsN = "";
-                    fse = false;
-                    tbcp = false;
-                }
-            }
-            else
-            {
-                textBox1.Text = "";
-                fsN = "";
-                fse = false;
-                tbcp = false;
-            }
-        }
-
         private void 열기OToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            if (tbcp == true)
+            try
             {
-                DialogResult answer = MessageBox.Show("변경사항을 저장하시겠습니까", "저장", MessageBoxButtons.YesNo);
-
-                if (answer == DialogResult.Yes)
+                if (isModified == true)
                 {
-                    if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+                    DialogResult answer = MessageBox.Show("변경된 내용을 저장하시겠습니까?", "저장", MessageBoxButtons.YesNo);
+                    if (answer == DialogResult.Yes)
                     {
-                        fsN = saveFileDialog1.FileName;
-                        System.IO.StreamWriter fs = new System.IO.StreamWriter(fsN, false, System.Text.Encoding.Default);
-                        fs.WriteLine(textBox1.Text);
-                        fs.Close();
-
-                        tbcp = false;
-                        fse = true;
-                    }
-
-                    if (openFileDialog1.ShowDialog() == DialogResult.OK)
-                    {
-                        System.IO.StreamReader fs = new System.IO.StreamReader(fsN, System.Text.Encoding.Default);
-                        textBox1.Text = fs.ReadToEnd();
-                        fs.Close();
-
-                        tbcp = false;
-                        fse = true;
+                        if (hasFileName == true)
+                        {
+                            System.IO.StreamWriter fs =
+                                    new System.IO.StreamWriter(sFileName, false, System.Text.Encoding.Default);
+                            fs.WriteLine(textBox1.Text);
+                            fs.Close();
+                            isModified = false;
+                        }
+                        else
+                        {
+                            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+                            {
+                                sFileName = saveFileDialog1.FileName;
+                                System.IO.StreamWriter fs =
+                                    new System.IO.StreamWriter(sFileName, false, System.Text.Encoding.Default);
+                                fs.WriteLine(textBox1.Text);
+                                fs.Close();
+                                hasFileName = true;
+                                isModified = false;
+                            }
+                        }
                     }
                 }
-                else
-                {
-                    if (openFileDialog1.ShowDialog() == DialogResult.OK)
-                    {
-                        System.IO.StreamReader fs = new System.IO.StreamReader(fsN, System.Text.Encoding.Default);
-                        textBox1.Text = fs.ReadToEnd();
-                        fs.Close();
-
-                        tbcp = false;
-                        fse = false;
-                    }
-                }
-            }
-            else
-            {
                 if (openFileDialog1.ShowDialog() == DialogResult.OK)
                 {
-                    System.IO.StreamReader fs = new System.IO.StreamReader(fsN, System.Text.Encoding.Default);
+                    sFileName = openFileDialog1.FileName;
+                    System.IO.StreamReader fs = new System.IO.StreamReader(sFileName, System.Text.Encoding.Default);
                     textBox1.Text = fs.ReadToEnd();
                     fs.Close();
-
-                    tbcp = false;
-                    fse = false;
+                    hasFileName = true;
+                    isModified = false;
                 }
             }
-        }
-
-        private void 저장SToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            if (fse == false)
+            catch
             {
-                DialogResult answer = MessageBox.Show("변경사항을 저장하시겠습니까", "저장", MessageBoxButtons.YesNo);
-
-                if (answer == DialogResult.Yes)
-                {
-                    if (saveFileDialog1.ShowDialog() == DialogResult.OK)
-                    {
-                        fsN = saveFileDialog1.FileName;
-                        System.IO.StreamWriter fs = new System.IO.StreamWriter(fsN, false, System.Text.Encoding.Default);
-                        fs.WriteLine(textBox1.Text);
-                        fs.Close();
-
-                        tbcp = false;
-                        fse = true;
-                    }
-                }
+                MessageBox.Show("열기를 하는 도중 이상이 발생했습니다.", "에러", MessageBoxButtons.OK, MessageBoxIcon.Warning);
             }
-            else
-            {
-                fsN = saveFileDialog1.FileName;
-                System.IO.StreamWriter fs = new System.IO.StreamWriter(fsN, false, System.Text.Encoding.Default);
-                fs.WriteLine(textBox1.Text);
-                fs.Close();
-
-                tbcp = false;
-            }
-        }
-
-        private void 끝내기XToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            Application.Exit();
-        }
-
-        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
-        {
-            if (tbcp == true)
-            {
-                DialogResult answer = MessageBox.Show("변경사항을 저장하시겠습니까", "저장", MessageBoxButtons.YesNoCancel);
-
-                if (answer == DialogResult.Yes)
-                {
-                    if (saveFileDialog1.ShowDialog() == DialogResult.OK)
-                    {
-                        fsN = saveFileDialog1.FileName;
-                        System.IO.StreamWriter fs = new System.IO.StreamWriter(fsN, false, System.Text.Encoding.Default);
-                        fs.WriteLine(textBox1.Text);
-                        fs.Close();
-                    }
-                }
-                else if (answer == DialogResult.Cancel)
-                {
-                    e.Cancel = true;
-                }
-            }
-        }
-
-        private void 잘라내기TToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            textBox1.Cut();
         }
 
         private void 복사CToolStripMenuItem_Click(object sender, EventArgs e)
@@ -187,35 +74,166 @@ namespace MemoPadWithEdit
             textBox1.Copy();
         }
 
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            isModified = true;
+        }
         private void 붙여넣기VToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            textBox1.Paste();
-        }
+            textBox1.Paste(); // 붙혀넣기
 
-        private void 삭제DToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            textBox1.Paste("");
         }
 
         private void 모두선택AToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            textBox1.SelectAll();
+            textBox1.SelectAll(); //모두 선택
+
+        }
+
+        private void 삭제DToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            textBox1.SelectedText = "";
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void 잘라내기TToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            textBox1.Cut();
+        }
+
+        private void 저장SToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (hasFileName == true)
+                {
+                    System.IO.StreamWriter fs =
+                                    new System.IO.StreamWriter(sFileName, false, System.Text.Encoding.Default);
+                    fs.WriteLine(textBox1.Text);
+                    fs.Close();
+                    isModified = false;
+                }
+                else
+                {
+                    if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+                    {
+                        sFileName = saveFileDialog1.FileName;
+                        System.IO.StreamWriter fs =
+                                    new System.IO.StreamWriter(sFileName, false, System.Text.Encoding.Default);
+                        fs.WriteLine(textBox1.Text);
+                        fs.Close();
+                        hasFileName = true;
+                        isModified = false;
+                    }
+                }
+            }
+            catch
+            {
+                MessageBox.Show("저장을 하는 도중 이상이 발생했습니다.", "에러", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void 새로만들기NToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (isModified == true)
+                {
+                    DialogResult answer = MessageBox.Show("변경된 내용을 저장하시겠습니까?", "저장", MessageBoxButtons.YesNo);
+
+                    if (answer == DialogResult.Yes)
+                    {
+                        if (hasFileName == true)
+                        {
+                            System.IO.StreamWriter fs =
+                                        new System.IO.StreamWriter(sFileName, false, System.Text.Encoding.Default);
+                            fs.WriteLine(textBox1.Text);
+                            fs.Close();
+                            isModified = false;
+                        }
+                        else
+                        {
+                            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+                            {
+                                sFileName = saveFileDialog1.FileName;
+                                System.IO.StreamWriter fs =
+                                    new System.IO.StreamWriter(sFileName, false, System.Text.Encoding.Default);
+                                fs.WriteLine(textBox1.Text);
+                                fs.Close();
+                                hasFileName = true;
+                                isModified = false;
+                            }
+                        }
+                    }
+                }
+
+                textBox1.Text = "";
+                isModified = false;
+                hasFileName = false;
+                sFileName = "";
+            }
+            catch
+            {
+                MessageBox.Show("새파일을 준비하는 도중 이상이 발생했습니다.", "에러", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void 끝내기XToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (isModified == true)
+                {
+                    DialogResult answer = MessageBox.Show("변경된 내용을 저장하시겠습니까?", "저장", MessageBoxButtons.YesNo);
+
+                    if (answer == DialogResult.Yes)
+                    {
+                        if (hasFileName == true)
+                        {
+                            System.IO.StreamWriter fs =
+                                    new System.IO.StreamWriter(sFileName, false, System.Text.Encoding.Default);
+                            fs.WriteLine(textBox1.Text);
+                            fs.Close();
+                            isModified = false;
+                        }
+                        else
+                        {
+                            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+                            {
+                                sFileName = saveFileDialog1.FileName;
+                                System.IO.StreamWriter fs =
+                                        new System.IO.StreamWriter(sFileName, false, System.Text.Encoding.Default);
+                                fs.WriteLine(textBox1.Text);
+                                fs.Close();
+                                hasFileName = true;
+                                isModified = false;
+                            }
+                        }
+
+                    }
+                }
+                Application.Exit();
+            }
+            catch
+            {
+                MessageBox.Show("끝내는 도중 이상이 발생했습니다.", "에러", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
         }
 
         private void 글꼴LToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            if (fontDialog1.ShowDialog() == DialogResult.Cancel)
-            {
+            if (fontDialog1.ShowDialog() != DialogResult.Cancel)
                 textBox1.Font = fontDialog1.Font;
-            }
         }
 
         private void 색깔CToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            if (colorDialog1.ShowDialog() == DialogResult.Cancel)
-            {
+            if (colorDialog1.ShowDialog() != DialogResult.Cancel)
                 textBox1.ForeColor = colorDialog1.Color;
-            }
         }
     }
 }
